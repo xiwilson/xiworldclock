a49e5445c6be3a662495de3f333f6c5b03134768
xiwilson <linzi.wilson@hotmail.com>
2025-03-06 20:38:08 +0000

    Added current time zone

--- javascript/index.js
+++ javascript/index.js
@@ -48,6 +48,9 @@ if (romeElement) {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
